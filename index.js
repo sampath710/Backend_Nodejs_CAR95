41f36548840a8ed741b888e6ef6111a6faaf7fd6
Sampath710 <msampathk086@gmail.com>
2024-10-22 23:26:24 +0530

    added path module

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
-const path = require('path')
 
 const app = express();
+app.use(cors())
 
 const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
-app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("MongoDB connected sucessfully!"))
--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -2,16 +2,21 @@
 const express = require('express');
 const firmController = require('../controllers/firmController');
 const verifyToken = require('../middlewares/verifyToken');
+const path = require('path');
 
 
 const router = express.Router()
 
-router.post('/add-firm', verifyToken, firmController.addFirm);
+router.get('/add-firm', verifyToken, firmController.addFirm);
 
-router.get('/uploads/:imageName', (req, res)=>{
+router.post('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    res.headersSent('Content-Type', 'image/jpeg');
-    res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
+    res.setHeader('Content-Type', 'image/jpeg'); // Set the correct header
+    res.sendFile(path.join(__dirname, '..', 'uploads', imageName), (err) => {
+        if (err) {
+            res.status(err.status).end();
+        }
+    });
 });
 
 
